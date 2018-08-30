27bfce0803b9ab185842106f1c3db764bd48e483
Jim Williams <jimwilliams@macbook-pro-7.local>
2018-08-30 09:32:19 -0500

    Initial Commit

---
+++ Video Game Library/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library
+//
+//  Created by Jim Williams on 8/30/18.
+//  Copyright © 2018 Visioncast Solutions. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
